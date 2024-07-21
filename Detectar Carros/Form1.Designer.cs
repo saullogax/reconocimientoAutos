80eae4c5e64e46a049ac3bfaff05cd019e66b0a9
Guillermo Gaxiola <saullogax@gmail.com>
2024-07-20 17:54:57 -0700

    reconoce carros

---
+++ Detectar Carros/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Detectar_Carros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 13);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(699, 431);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(173, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cargar Video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(390, 450);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Detectar Carros";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 507);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Detectar Carros/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Threading;
+
+namespace Detectar_Carros
+{
+    public partial class Form1 : Form
+    {
+
+
+        //SE DECLARAN VARIABLES DE CAPTURA
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        List<Image> imgList = new List<Image>();
+        int i = 0;
+        int total = 0;
+
+        private CascadeClassifier _CascadeClassifier;
+        public Form1()
+        {
+            InitializeComponent();
+            _CascadeClassifier = new CascadeClassifier(Application.StartupPath + "/cars2.xml");
+        }
+
+        private void DisplayWebCam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                //CvInvoke.Resize(frame, frame, imageBox1.Size);
+                imageBox1.Image = frame;
+
+                using (var ImageFrame = frame.ToImage<Bgr, Byte>())
+                {
+                    if (ImageFrame != null)
+                    {
+                        var grayFrame = ImageFrame.Convert<Gray, Byte>();
+
+                        var cars = _CascadeClassifier.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+
+                        foreach (var car in cars)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(car, new Bgr(Color.Red), 3);
+                        }
+
+                    }
+                    imageBox1.Image = ImageFrame;
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                //SE CARGARA EL VIDEO
+                _capture = new VideoCapture(open.FileName);
+                _captureThread = new Thread(DisplayWebCam);
+                _captureThread.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
